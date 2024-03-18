66c5c82ac63d5062608cbc1f72b5e3c90c7b7dc7
navneetpal01 <navneetpalgithub@gmail.com>
2024-03-18 15:57:10 +0530

    18 march

--- app/src/main/java/com/example/firebase_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/firebase_jetpackcompose/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.firebase_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.firebase_jetpackcompose.ui.theme.FirebaseJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +12,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FirebaseJetpackComposeTheme {
-
+                Login()
             }
         }
     }
+}
+
+@Composable
+fun Login(){
+
 }
 
